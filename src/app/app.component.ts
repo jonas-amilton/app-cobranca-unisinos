db7528b4775effa8ec237b4c5481f419cfdd2d96
Jonas Silva <jonas86@edu.unisinos.br>
2023-09-19 03:37:42 -0300

    [update] pages em componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Login', url: '/login', icon: 'log-in' },
+    { title: 'Cadastro de usuários', url: '/#', icon: 'person-add' },
+    { title: 'Lista de Clientes', url: '/#', icon: 'list' },
+    { title: 'Pagamento', url: '/#', icon: 'card' },
+    { title: 'Trash', url: '/#', icon: 'trash' },
+    { title: 'Spam', url: '/#', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
